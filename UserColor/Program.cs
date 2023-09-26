da0827196971c7093a417b134bb5990ea67e61c0
iZnoG0uD <zelesnac@gmail.com>
2023-09-26 17:53:26 +0400

    Добавьте файлы проекта.

---
+++ UserColor/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UserColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tНапишите свой любимый цвет на английском с маленькой буквы: ");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    break;
+
+            }
+        }
+    }
+}
